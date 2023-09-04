70c72bf5c07bfbea375880b63b2061daf8b1ec5d
gali gershtein <213337868@idf.il>
2023-09-04 14:34:33 +0300

    add vuetify

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -31,8 +31,8 @@ export default {
       url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
       attribution:
         '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-      zoom: 15,
-      center: [51.505, -0.159],
+      zoom: 5,
+      center: [31.673109, 40.125761],
       markerLatLng: [51.504, -0.159]
     }
   }
