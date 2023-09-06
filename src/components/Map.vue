5ee4c5f5323d9b3fd23ea403be7bae366acd6836
yairbendavi <ybenda208@gmail.com>
2023-09-06 11:06:26 +0300

    minor changes

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -81,68 +81,86 @@ export default {
       Get Location
     </v-btn>
     {{ this.lat }} , {{ this.lng }}
-    
-    <v-btn @click="getLocation(48.866667, 2.333333, 'Paris')" prepend-icon="$vuetify" variant="tonal" style="margin: 10px;">
+
+    <v-btn
+      @click="getLocation(48.866667, 2.333333, 'Paris')"
+      prepend-icon="$vuetify"
+      variant="tonal"
+      style="margin: 10px;"
+    >
       Paris
     </v-btn>
 
-    <v-btn @click="getLocation(40.416775, -3.703790, 'Madrid')" prepend-icon="$vuetify" variant="tonal"
-      style="margin: 10px;">
+    <v-btn
+      @click="getLocation(40.416775, -3.70379, 'Madrid')"
+      prepend-icon="$vuetify"
+      variant="tonal"
+      style="margin: 10px;"
+    >
       Madrid
     </v-btn>
-    
-    <v-btn @click="getLocation(undefined, undefined, undefined,true,false)" prepend-icon="$vuetify" variant="tonal"
-      style="margin: 10px;">
+
+    <v-btn
+      @click="getLocation(undefined, undefined, undefined, true, false)"
+      prepend-icon="$vuetify"
+      variant="tonal"
+      style="margin: 10px;"
+    >
       Move
     </v-btn>
 
-    <v-btn @click="getLocation(undefined, undefined, undefined,false)" prepend-icon="$vuetify" variant="tonal"
-      style="margin: 10px;">
+    <v-btn
+      @click="getLocation(undefined, undefined, undefined, false)"
+      prepend-icon="$vuetify"
+      variant="tonal"
+      style="margin: 10px;"
+    >
       Stop Move
     </v-btn>
 
-
-    <div id="mapContainer" ref="mapContainer" style="width: 99%; height: 77vh; margin: 10px"></div>
-
+    <div
+      id="mapContainer"
+      ref="mapContainer"
+      style="width: 99%; height: 77vh; margin: 10px"
+    ></div>
   </div>
 </template>
 <script>
-import LMovingMarker from 'vue2-leaflet-movingmarker'
+import LMovingMarker from "vue2-leaflet-movingmarker";
 import L from "leaflet";
-import plane from '../assets/plane.png'
-import shadow from '../assets/leaf-shadow.png'
+import plane from "../assets/plane.png";
+import shadow from "../assets/leaf-shadow.png";
 import socket from "../../config/socketService";
 
-let last_marker = null
+let last_marker = null;
 var greenIcon = L.icon({
-            iconUrl: plane,
-            shadowUrl: shadow,
+  iconUrl: plane,
+  shadowUrl: shadow,
 
-            iconSize: [50, 55], // size of the icon
-            shadowSize: [50, 55], // size of the shadow
-            iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
-            shadowAnchor: [30, 62],  // the same for the shadow
-            // popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
-          });
-
+  iconSize: [50, 55], // size of the icon
+  shadowSize: [50, 55], // size of the shadow
+  iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+  shadowAnchor: [30, 62] // the same for the shadow
+  // popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
 
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-  iconUrl: require('leaflet/dist/images/marker-icon.png'),
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
+  iconUrl: require("leaflet/dist/images/marker-icon.png"),
+  shadowUrl: require("leaflet/dist/images/marker-shadow.png")
 });
 
 export default {
-  mounted:
-    function () {
-      this.map.value = L.map('mapContainer').setView([51.505, -0.09], 13);
-      L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-        maxZoom: 19,
-        attribution:
-          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-      }).addTo(this.map.value);
-       socket.connect();
+  mounted: function() {
+    this.map.value = L.map("mapContainer").setView([51.505, -0.09], 13);
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      maxZoom: 19,
+      attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(this.map.value);
+
+    socket.connect();
     console.log("checkkk");
     socket.on("connect", () => {
       console.log("Connected to WebSocket");
@@ -150,12 +168,10 @@ export default {
 
     socket.on("message", data => {
       console.log("Received message:", data);
-      this.getLocation();
+      this.getLocation(data.x, data.y); // here the lat lon info need to be shown
       // Handle incoming messages as needed
     });
-      
-
-    },
+  },
 
   data: () => ({
     lng: 0,
@@ -163,45 +179,43 @@ export default {
     map: {}
   }),
   methods: {
-    getLocation(lat, lon, name,move) {
+    getLocation(lat, lon, name, move) {
       if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition((position) => {
-          if(move != true){
-
+        navigator.geolocation.getCurrentPosition(position => {
+          if (move != true) {
             this.lat = position.coords.latitude;
             this.lng = position.coords.longitude;
           }
 
           if (lat != undefined && lon != undefined && name != undefined) {
-            this.lat = lat
-            this.lng = lon
+            this.lat = lat;
+            this.lng = lon;
           }
-
 
           this.map.value.setView([this.lat, this.lng], 13);
 
-          if(move == true){
-            this.lat = this.lat - 0.0045
-            this.lng = this.lng - 0.0045
-            this.map.value.removeLayer(last_marker)
-            console.log(move)
-            
+          if (move == true) {
+            this.lat = this.lat - 0.0045;
+            this.lng = this.lng - 0.0045;
+            this.map.value.removeLayer(last_marker);
+            console.log(move);
           }
-          
-          last_marker = L.marker([this.lat,this.lng], { icon: greenIcon }).addTo(this.map.value);
-          if(move == true){this.getLocation(undefined,undefined,undefined,true)}
+
+          last_marker = L.marker([this.lat, this.lng], {
+            icon: greenIcon
+          }).addTo(this.map.value);
+          if (move == true) {
+            this.getLocation(undefined, undefined, undefined, true);
+          }
 
           // L.marker([this.lat, this.lng])
           //   .addTo(this.map.value);
-            // .on("dragend", (event) => {
-            //   console.log(event)
-            // });
-
-
-
+          // .on("dragend", (event) => {
+          //   console.log(event)
+          // });
         });
       }
-    },
+    }
     // Icons
   },
   components: {
